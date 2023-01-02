76ef1bed5a523633216f95b99ee65bead86936f7
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-01-03 01:02:48 +0900

    파일 구분

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
+const logger = require('morgan');
 
 
 const port = 8080;
@@ -10,14 +10,13 @@ const app = express();
 
 // 바디 크기 지정
 app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb', extended: false }));
 app.use(cors({
     origin: 'http://localhost:8081',
     credentials: true
 }));
-// 쿠키 관련 설정
 app.use(cookieParser());
 
-// 기본 값 반환
 app.get('/',(req,res)=> {
     res.send("요청 성공");
 });
