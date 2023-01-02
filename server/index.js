a70ca1d03eda0379d7b2a590524e2b22171a578b
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-01-02 18:26:39 +0900

    수정필요사항

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+
+
 const port = 8080;
 const app = express();
-const cors = require('cors');
 
-app.get('/',(req,res)=> {
-    res.send("서버시작");
-})
+
+// 바디 크기 지정
+app.use(express.json({limit: '100mb'}));
 app.use(cors({
     origin: 'http://localhost:8081',
     credentials: true
-}))
+}));
+// 쿠키 관련 설정
+app.use(cookieParser());
+
+// 기본 값 반환
+app.get('/',(req,res)=> {
+    res.send("요청 성공");
+});
+
 app.listen(port, () => {
     console.log('Example app listening on port');
-})
+});
