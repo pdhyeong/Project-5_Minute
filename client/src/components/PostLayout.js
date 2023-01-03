7b8b67d26777a8236274672f8864abb316829f33
oreozzang <93peace@naver.com>
2023-01-03 23:00:52 +0900

    feat: mypage

--- client/src/components/PostLayout.js
+++ client/src/components/PostLayout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CreatePost from './CreatePost';
 import Post from './Post';
 
 const PostLayout = () => {
     return (
         <div className='postlayout'>
+            <CreatePost></CreatePost>
             <Post></Post>
             <Post></Post>
             <Post></Post>
--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import profile from '../assets/레오쓰.jpeg'
 
 const Mypage = () => {
+    const [tap,setTap] = useState(0);
+
     return (
-        <div>
-            마이페이지입니다.
+        <div className='mypage'>
+            <div className='mypage__background'>
+                <div className='mypage__background__profile__img'>
+                    <img src={profile}></img>
+                </div>
+            </div>
+            <div className='mypage__background__spacer'>
+
+            </div>
+            <div className='mypage__profile__contents'>
+                <h3>zinoopark</h3>
+                <span>@zinoo_park</span>
+                <p>가입일: 2022년 10월</p>
+                <div>
+                    획득 뱃지 <span>a</span><span>b</span><span>c</span><span>d</span>
+                </div>
+            </div>
+            <div className='mypage__contents'>
+                <div className='mypage__contents__tap'>
+                    <div onClick={()=>{setTap(0)}} className={tap===0?'mypage__contents__tap__item tap-hover':'mypage__contents__tap__item'}>RANK</div>
+                    <div onClick={()=>{setTap(1)}} className={tap===1?'mypage__contents__tap__item tap-hover':'mypage__contents__tap__item'} >POST</div>
+                </div>
+            </div>
         </div>
     );
 };
